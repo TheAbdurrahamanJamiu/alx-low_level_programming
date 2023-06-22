d8f726894f0051d471ae7ce7949a053c49fcab2c
TheAbduurrahamanJamiu <abdurrahamanjamiu75outlook>
2023-06-21 20:24:57 -0700

    adds to integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add = Adds Twp values
+ * @n1: The first number to be completed
+ * @n2: The second number to be completed
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
