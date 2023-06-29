b2f926edae85345a6815336abb4bc765c843dd10
TheAbduurrahamanJamiu <abdurrahamanjamiu75outlook>
2023-06-28 20:54:18 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,27 +7,28 @@
  */
 
 void puts_half(char *str)
-
+{
 	int j = 0;
 	int k;
+
 	while (str[j] != '\0')
 	{
-	j++
+	j++;
 	}
 	if (j % 2 == 1)
 	{
-	k = (J - 1) / 2;
-	K += 1;
+	k = (j - 1) / 2;
+	k += 1;
 	}
 	else
 	{
 	k = j / 2;	
 	}
 	
-	for (; k < j k++)
+	for (; k < j; k++)
 	{
 	_putchar(str[k]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 
 }
