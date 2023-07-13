7298b3b4150b051a09083eb99aea8dbba74d732f
TheAbduurrahamanJamiu <abdurrahamanjamiu75outlook>
2023-07-12 18:45:39 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,34 +1,33 @@
-#include <stdlib.h>
+#include <stdlib.h> 
 #include "main.h"
- 
+
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
  * Return: number of words
  */
- 
+
 int count_word(char *s)
 {
 	int flag, c, w;
 	flag = 0;
-	w = 0;
+	w = 0; 
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
-		{
 			flag = 0;
-		}
 		else if (flag == 0)
 		{
 			flag = 1;
 			w++;
 		}
-		return (w);
 	}
+		return (w);
+}
 
 /**
  * **strtow - splits a string into words
- * @str: string to split
+ * * @str: string to split
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
@@ -64,9 +63,7 @@ char **strtow(char *str)
 			}
 		}
 		else if (c++ == 0)
-		{
 			start = i;
-		}
 	}
 	matrix[k] = NULL;
 	return (matrix);
